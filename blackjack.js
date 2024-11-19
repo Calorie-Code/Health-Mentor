9cd5895ae48af4e37f018010e8e2e6db548693bf
tony24123 <tjdduswns01@naver.com>
2024-11-19 23:25:37 +0900

    js 랜덤카드 뽑기 기능 추가

--- blackjack.js
+++ blackjack.js
@@ -1,321 +1,361 @@
 
 
 //Full array of possible cards
-let cards;
-
-function getCards(){
-	cards = [
+let cards=[
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "2",
 			src: "Clubs-2.png",
 			value: 2
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "3",
 			src: "Clubs-3.png",
 			value: 3
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "4",
 			src: "Clubs-4.png",
 			value: 4
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "5",
 			src: "Clubs-5.png",
 			value: 5
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "6",
 			src: "Clubs-6.png",
 			value: 6
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "7",
 			src: "Clubs-7.png",
 			value: 7
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "8",
 			src: "Clubs-8.png",
 			value: 8
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "9",
 			src: "Clubs-9.png",
 			value: 9
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "10",
 			src: "Clubs-10.png",
 			value: 10
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "ace",
 			src: "Clubs-Ace.png",
 			value: 11
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "jack",
 			src: "Clubs-Jack.png",
 			value: 10
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "king",
 			src: "Clubs-King.png",
 			value: 10
 		},
 		{ 
-			suit: "clubs",
+			suit: 1,
 			name: "queen",
 			src: "Clubs-Queen.png",
 			value: 10
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "2",
 			src: "Diamond-2.png",
 			value: 2
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "3",
 			src: "Diamond-3.png",
 			value: 3
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "4",
 			src: "Diamond-4.png",
 			value: 4
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "5",
 			src: "Diamond-5.png",
 			value: 5
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "6",
 			src: "Diamond-6.png",
 			value: 6
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "7",
 			src: "Diamond-7.png",
 			value: 7
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "8",
 			src: "Diamond-8.png",
 			value: 8
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "9",
 			src: "Diamond-9.png",
 			value: 9
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "10",
 			src: "Diamond-10.png",
 			value: 10
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "ace",
 			src: "Diamond-Ace.png",
 			value: 11
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "jack",
 			src: "Diamond-Jack.png",
 			value: 10
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "king",
 			src: "Diamond-King.png",
 			value: 10
 		},
 		{ 
-			suit: "diamonds",
+			suit: 2,
 			name: "queen",
 			src: "Diamond-Queen.png",
 			value: 10
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "2",
 			src: "Hearts-2.png",
 			value: 2
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "3",
 			src: "Hearts-3.png",
 			value: 3
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "4",
 			src: "Hearts-4.png",
 			value: 4
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "5",
 			src: "Hearts-5.png",
 			value: 5
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "6",
 			src: "Hearts-6.png",
 			value: 6
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "7",
 			src: "Hearts-7.png",
 			value: 7
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "8",
 			src: "Hearts-8.png",
 			value: 8
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "9",
 			src: "Hearts-9.png",
 			value: 9
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "10",
 			src: "Hearts-10.png",
 			value: 10
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "ace",
 			src: "Hearts-Ace.png",
 			value: 11
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "jack",
 			src: "Hearts-Jack.png",
 			value: 10
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "king",
 			src: "Hearts-King.png",
 			value: 10
 		},
 		{ 
-			suit: "hearts",
+			suit: 3,
 			name: "queen",
 			src: "Hearts-Queen.png",
 			value: 10
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "2",
 			src: "Spades-2.png",
 			value: 2
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "3",
 			src: "Spades-3.png",
 			value: 3
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "4",
 			src: "Spades-4.png",
 			value: 4
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "5",
 			src: "Spades-5.png",
 			value: 5
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "6",
 			src: "Spades-6.png",
 			value: 6
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "7",
 			src: "Spades-7.png",
 			value: 7
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "8",
 			src: "Spades-8.png",
 			value: 8
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "9",
 			src: "Spades-9.png",
 			value: 9
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "10",
 			src: "Spades-10.png",
 			value: 10
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "ace",
 			src: "Spades-Ace.png",
 			value: 11
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "jack",
 			src: "Spades-Jack.png",
 			value: 10
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "king",
 			src: "Spades-King.png",
 			value: 10
 		},
 		{ 
-			suit: "spades",
+			suit: 4,
 			name: "queen",
 			src: "Spades-Queen.png",
 			value: 10
 		}
-	]
+	];
+
+
+
+
+
+// console.log(randomCardNum);
+// console.log(randomCardShape);
+// console.log(cards[0].suit);
+let newRandomCard
+let newArr = [];
+
+const $dealerCard = document.querySelector('.dealer-card');
+
+console.log($dealerCard);
+
+//랜덤카드 만들기
+function RandomCard(){
+	let randomCardShape = Math.floor(Math.random() * 4) + 1; //랜덤 문양 
+	
+	let randomCardNum = Math.floor(Math.random() * 13) + 1; // 랜덤 숫자
+	console.log(randomCardNum);
+ 
+	//newCardList = 랜덤문양 카드 모두 담은 배열
+	let newCardList = cards.filter(card => card.suit === randomCardShape);
+	//newRandomCard = 랜덤으로 골라낸 카드
+	newRandomCard = newCardList[randomCardNum-1];
+
+	// console.log(newRandomCard);
+	return newRandomCard;	
+	//  newCardList.filter(card => card.value === randomCardNum);	 
 }
+
+//게임초기세팅
+function startTabel() {
+	// while(newArr.length <= 3){
+	// 	RandomCard();
+	// 	if(newArr)
+	// 	newArr.push(newRandomCard);
+	// }
+}
+
+// for(){ 랜덤카드를 일단 만든다. 그걸 특정 메모리에 넣어 2번째를 만들어 이제부터 비교가 들어간다. 같으면 continue }
+// startTabel();
+// console.log(newArr);
