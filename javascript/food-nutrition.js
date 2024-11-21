140222c37ec4d1bc59b7f456433784fc69a71d5b
futuredeveloper41 <mj10283@naver.com>
2024-11-21 20:12:38 +0900

    food-nutrition 목요일까지 작업분

--- javascript/food-nutrition.js
+++ javascript/food-nutrition.js
@@ -1,3 +1,12 @@
+// =============전역 변수 =============== //
+// API 주소
+const apiKey = "ccStZzcWOtwXEPyP7883p9z3cpgynAtuqu2jduhjkmFE0POBfxWkCOAvSjMPvqOeJgAK8IgXTzwo%2B6rZfQduZQ%3D%3D";
+const pageNo = 1;
+const numberOfRows = 100;
+
+
+let isModalMode = false;
+
 // ========= DOM =========== //
 // 메인 컨테이너
 const $mainContainer = document.querySelector('.main-container');
@@ -11,6 +20,9 @@ const $resultDiv = document.querySelector('.main-container .result');
 const $mainContainerTextBox = document.querySelector('.main-container .text-box');
 // no result 텍스트 
 const $noResult = document.querySelector('.main-container .no-result');
+// modal
+const $modalOverlay = document.querySelector('.modal-overlay');
+const $modalCloseBtn = document.querySelector('.modal-overlay .close-btn');
 
 
 // ============= 함수 ============= //
@@ -54,20 +66,13 @@ function throttle(func, limit) {
 async function searchProduct(e) {
   // input 박스 사용자 입력값 가져오기
   const inputFood = $productNameInput.value;
-
-  // 식약처 DB에서 해당 이름 가진 제품 검색하기
-  // 1. url 구조
-  const apiKey =
-    "ccStZzcWOtwXEPyP7883p9z3cpgynAtuqu2jduhjkmFE0POBfxWkCOAvSjMPvqOeJgAK8IgXTzwo%2B6rZfQduZQ%3D%3D";
-  const pageNo = 1;
-  const numberOfRows = 100;
-  const foodName = inputFood;
   const url = `https://apis.data.go.kr/1471000/FoodNtrCpntDbInfo01/getFoodNtrCpntDbInq01?serviceKey=${apiKey}&pageNo=${pageNo}&numOfRows=${numberOfRows}&type=json&FOOD_NM_KR=${inputFood}`;
 
-  // 2. API 검색
+  // 2. 식약처 DB API 검색
   const response = await fetch(url);
   const apiData = await response.json();
   const searchResult = apiData.body.items;
+  console.log(searchResult);
 
   // !! searchResult가 없다 == 검색결과가 없음
   //  => 검색 결과 없을 때 효과
@@ -101,19 +106,20 @@ async function searchProduct(e) {
       MAKER_NM: manufacturer,
     } = item;
 
+    // 업체명 표기 용
+    const manufacturerText = manufacturer || '- ';
+
 
     // 태그 생성하기
     $newDiv = document.createElement("div");
     $newDiv.classList.add("product-card");
+    $newDiv.dataset.foodName = foodName;
     $newDiv.dataset.code = code;
-    $newDiv.innerHTML = `
-      <span class=foodName data-code="${code}">${foodName} (업체명: ${manufacturer || '- '})</span>
-    `;
+    $newDiv.innerHTML = `<span class="foodName">${foodName} (업체명: ${manufacturerText})</span>`;
     $resultDiv.append($newDiv);
   });
   // 메뉴 박스 크기 조정
   $mainContainerTextBox.classList.add('search-mode');
-  console.log($mainContainerTextBox);
 
   // 검색 결과 박스 열기
   $resultDiv.style.display = 'block';
@@ -123,17 +129,59 @@ async function searchProduct(e) {
 
 // 검색 입력창에 입력 후 엔터 치면 검색버튼 누른 것과 같은 효과 주는 핸들러
 function productNameInputandEnterHandler(e) {
-  if (e.key === 'Enter') {
+  if (e.key === 'Enter') {  
     searchProduct(e);
   }
 }
 
+
+// 상세보기 모달 클로즈 이벤트
+// 1. close 버튼
+function closeModal(e) {
+  $modalOverlay.classList.remove('show');
+};
+
+
+
+// 2. 모달 활성화 상태에서 esc
+async function escCloseModal(e) {
+  if (!isModalMode) return;  // 모달 활성화상태에서만 작동
+  if (e.key !== 'Escape') return;
+  closeModal();
+  isModalMode = false; 
+}
+
 // 검색 결과 나오는 이벤트
-function showDetail(e) {
+async function showDetail(e) {
+  isModalMode = true;
   console.log(e.target);
   if (e.target.classList.contains('product-card')) return;
-  console.log('여긴 akwdma');
+
+  // 모달 활성화
+  console.log('클릭');
+  $modalOverlay.classList.add('show');
+  console.log($modalOverlay);
+
+  // 해당 제품의 정보 가져오기 위해 부모.product에서 정보 가져오기(식별자)
+  $closestProductCard = e.target.closest('.product-card');
+  console.log($closestProductCard);
+
+  // fetch 
+  // 1. 경로 세팅
+  const inputFood = $closestProductCard.dataset.foodName;
+  const url = `https://apis.data.go.kr/1471000/FoodNtrCpntDbInfo01/getFoodNtrCpntDbInq01?serviceKey=${apiKey}&pageNo=${pageNo}&numOfRows=${numberOfRows}&type=json&FOOD_NM_KR=${inputFood}`;
+  // 2. 불러오기
+  const response = await fetch(url);
+  const apiData = await response.json();
+  const searchResult = apiData.body.items;
+  // 3. 업체명까지 같은 제품으로 찾기
+  const finalResult = searchResult.find(item => item.FOOD_CD === $closestProductCard.dataset.code);
+  console.log(finalResult);
+  // 4. 제품명 : ㅇ
+  const $title = document.querySelector('.modal-overlay .modal-body');
+  $title.textContent = 'gggg';
 }
+
 
 
 // ================ 이벤트 등록 ================== //
@@ -144,8 +192,17 @@ $productNameInput.addEventListener('input', debounce(searchProduct, 200));
 // 검색 이벤트 (3) : input 에서 enter 누를 때
 $productNameInput.addEventListener('keydown', productNameInputandEnterHandler);
 
-// 결과 나오는 이벤트
+// 결과 상세보기 이벤트
 $resultDiv.addEventListener('click', showDetail);
+
+// 결과 상세보기 close 이벤트 (1) 클로즈 버튼 누를 시
+$modalCloseBtn.addEventListener('click', closeModal);
+
+// 결과 상세보기 close 이벤트 (2) 모달 열린 상태에서 esc 누를 시
+document.addEventListener('keydown', escCloseModal);
+
+ 
+
 
 
 
